5401d70f6d5d37eed6c43105f4312d7bc9eeb0b2
Paula <cedones@gmail.com>
2020-05-06 18:14:57 -0300

    visualizar numeros en display

---
+++ js/app.js
@@ -0,0 +1,61 @@
+document.querySelector('#n1').addEventListener('click', n1);
+document.querySelector('#n2').addEventListener('click', n2);
+document.querySelector('#n3').addEventListener('click', n3);
+document.querySelector('#n4').addEventListener('click', n4);
+document.querySelector('#n5').addEventListener('click', n5);
+document.querySelector('#n6').addEventListener('click', n6);
+document.querySelector('#n7').addEventListener('click', n7);
+document.querySelector('#n8').addEventListener('click', n8);
+document.querySelector('#n9').addEventListener('click', n9);
+document.querySelector('#n0').addEventListener('click', n0);
+
+function n1 () {
+    let actual = document.querySelector('#resultado').innerHTML;
+    let sumado = document.querySelector('#n1').innerHTML;
+    document.querySelector('#resultado').innerHTML = actual + sumado;
+}
+function n2 () {
+    let actual = document.querySelector('#resultado').innerHTML;
+    let sumado = document.querySelector('#n2').innerHTML;
+    document.querySelector('#resultado').innerHTML = actual + sumado;
+}
+function n3 () {
+    let actual = document.querySelector('#resultado').innerHTML;
+    let sumado = document.querySelector('#n3').innerHTML;
+    document.querySelector('#resultado').innerHTML = actual + sumado;
+}
+function n4 () {
+    let actual = document.querySelector('#resultado').innerHTML;
+    let sumado = document.querySelector('#n4').innerHTML;
+    document.querySelector('#resultado').innerHTML = actual + sumado;
+}
+function n5 () {
+    let actual = document.querySelector('#resultado').innerHTML;
+    let sumado = document.querySelector('#n5').innerHTML;
+    document.querySelector('#resultado').innerHTML = actual + sumado;
+}
+function n6 () {
+    let actual = document.querySelector('#resultado').innerHTML;
+    let sumado = document.querySelector('#n6').innerHTML;
+    document.querySelector('#resultado').innerHTML = actual + sumado;
+}
+function n7 () {
+    let actual = document.querySelector('#resultado').innerHTML;
+    let sumado = document.querySelector('#n7').innerHTML;
+    document.querySelector('#resultado').innerHTML = actual + sumado;
+}
+function n8 () {
+    let actual = document.querySelector('#resultado').innerHTML;
+    let sumado = document.querySelector('#n8').innerHTML;
+    document.querySelector('#resultado').innerHTML = actual + sumado;
+}
+function n9 () {
+    let actual = document.querySelector('#resultado').innerHTML;
+    let sumado = document.querySelector('#n1').innerHTML;
+    document.querySelector('#resultado').innerHTML = actual + sumado;
+}
+function n0 () {
+    let actual = document.querySelector('#resultado').innerHTML;
+    let sumado = document.querySelector('#n0').innerHTML;
+    document.querySelector('#resultado').innerHTML = actual + sumado;
+}
